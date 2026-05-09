693fe83eed379813a3bf7bfb92851d0e91fefb2e
Marcus Vinícius Rocha Marçal <36481692+marcusmarcal@users.noreply.github.com>
2026-05-09 08:26:51 +0100

    MANUAL: deleting stuff

--- teste.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "PHP OK: " . phpversion();
-echo "<br>curl: " . (function_exists('curl_init') ? 'disponível' : 'BLOQUEADO');
-echo "<br>file_put_contents: " . (is_writable('playlists.json') ? 'gravável' : 'SEM PERMISSÃO');
